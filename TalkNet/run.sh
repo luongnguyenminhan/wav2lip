9e2a663c4dd223236d981aa97e99c6e72c21965a
luongnguyenminhan <ledinhnguyen00@gmail.com>
2024-07-31 23:40:56 +0700

    add data 4, 5 videos

--- TalkNet/run.sh
+++ TalkNet/run.sh
@@ -6,7 +6,8 @@
 # video_link="https://drive.google.com/file/d/1kddSO1217sD42GZVH81IbjzGC7_Pptgh/view?usp=drive_link"
 # video_link="https://drive.google.com/file/d/1aA1w0YLdl6DAdzhI8v6q_bRFsVZ0J6Ul/view?usp=sharing" # videos
 # video_link="https://drive.google.com/file/d/1iaOJXt-weJEMZg5vlO-S2MNBQRIk1B5f/view?usp=drive_link" # data
-video_link="https://drive.google.com/file/d/1mbKfarkwWv0y14oqB9SfW4Lm-u-jfcjF/view?usp=sharing" # data 3
+# video_link="https://drive.google.com/file/d/1mbKfarkwWv0y14oqB9SfW4Lm-u-jfcjF/view?usp=sharing" # data 3
+video_link="https://drive.google.com/file/d/1HscH8xDiUXfnSZteJ2Cq0rPhgkD79rk8/view?usp=drive_link" # data 4
 name="data"
 
 # gdown --fuzzy $video_link -O $name.zip
@@ -45,22 +46,22 @@ for video in "${video_files[@]}"; do
     echo "****** Done detect talking face $video_name ******"
 done
 
-mv "$data_folder"/*.mp4 $raw_data
+# mv "$data_folder"/*.mp4 $raw_data
 
-# move all pycrop
-video_outputs=($(ls "$data_folder"/))
-for video_output in "${video_outputs[@]}"; do
-    # get video name
-    echo $video_output
+# # move all pycrop
+# video_outputs=($(ls "$data_folder"/))
+# for video_output in "${video_outputs[@]}"; do
+#     # get video name
+#     echo $video_output
 
-    # echo "Preprocessing video: $video_name"
+#     # echo "Preprocessing video: $video_name"
 
-    # Run the processing command
-    echo "****** Start Moving file ******"
-    # Rename file before moving
-    for file in $data_folder/$video_output/pycrop/*.avi; do
-        mv $file ${file%.wav}_$video_output.avi
-    done
-    mv $data_folder/$video_output/pycrop/*.avi $output_folder
-    echo "****** Done Moving file ******"
-done
+#     # Run the processing command
+#     echo "****** Start Moving file ******"
+#     # Rename file before moving
+#     for file in $data_folder/$video_output/pycrop/*.avi; do
+#         mv $file ${file%.wav}_$video_output.avi
+#     done
+#     mv $data_folder/$video_output/pycrop/*.avi $output_folder
+#     echo "****** Done Moving file ******"
+# done
